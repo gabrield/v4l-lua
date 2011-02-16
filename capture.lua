a0a253d7b86be6a52bcd786a29e2d4868a7d8a99
Gabriel Duarte <confusosk8@gmail.com>
2011-02-16 16:27:21 -0200

    :)

--- capture.lua
+++ capture.lua
@@ -6,21 +6,19 @@ require "table"
 function saveimg(img)
  file = io.open("image.ppm", "w+")
  file:write("P3\n".. w .. " " .. h .."\n255\n") -- RGB IMAGE
- 
  for i=0,#img do
     local p = a[i] .. "\n"  
     file:write(p)
   end
- 
   file:close()
 end
 
 camera = #arg
 
 if camera < 1 then
- camera = "/dev/video0"
+  camera = "/dev/video0"
 else
- camera = arg[1]
+  camera = arg[1]
 end
 
 print(camera)
@@ -28,8 +26,8 @@ print(camera)
 dev = v4l.open(camera)
 
 if dev < 0 then
- print("camera not found")
- os.exit(0)
+  print("camera not found")
+  os.exit(0)
 end
 
 w, h = v4l.widht(), v4l.height()
@@ -37,12 +35,11 @@ w, h = v4l.widht(), v4l.height()
 print(w .. "x" .. h)
 
 for i=0,10 do
- a = v4l.getframe()
+   a = v4l.getframe()
 end
 
 saveimg(a)
 a = nil
-
 
 v4l.close(dev);
 
